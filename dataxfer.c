1e4f226321fb38aeff18cd6a0d47a04ce09905bc
Corey Minyard <cminyard@mvista.com>
2013-07-26 18:49:00 -0500

    Fix hex string handling
    Fix a bug in format strings where hex element (\x)
    would have the wrong values for a-f.

--- dataxfer.c
+++ dataxfer.c
@@ -1022,9 +1022,9 @@ from_hex_digit(char c)
     if ((c >= '0') && (c <= '9'))
 	return c - '0';
     if ((c >= 'A') && (c <= 'F'))
-	return c = 'A' + 10;
+	return c - 'A' + 10;
     if ((c >= 'a') && (c <= 'f'))
-	return c = 'a' + 10;
+	return c - 'a' + 10;
     return 0;
 }
 
