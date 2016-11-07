be5920b95854c917f5ee642d7485af9bff0a7fdd
Corey Minyard <cminyard@mvista.com>
2016-11-07 08:33:15 -0700

    Handle locking correctly when shutting down
    During shutdown, the port lock was not taken before calling
    change_port_state(), but that lock is required.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- dataxfer.c
+++ dataxfer.c
@@ -3450,6 +3450,7 @@ shutdown_ports(void)
     while (port != NULL) {
 	port->config_num = -1;
 	next = port->next;
+	LOCK(port->lock);
 	change_port_state(NULL, port, PORT_DISABLED);
 	shutdown_port(port, "program shutdown");
 	port = next;
