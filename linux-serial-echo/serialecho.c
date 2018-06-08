bd5d99d71b162dfc7c12639e428e8c45472e075e
Corey Minyard <cminyard@mvista.com>
2018-06-07 20:42:00 -0500

    Add a pipe option to the serialecho Linux driver
    This creates two devices and a two-way pipe between them.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- linux-serial-echo/serialecho.c
+++ linux-serial-echo/serialecho.c
@@ -1,16 +1,22 @@
 // SPDX-License-Identifier: GPL-2.0+
 
 /*
- * serialecho - Emulate a serial device in loopback
+ * serialecho - Emulate a serial device in loopback or pipe
  *
  * This attempts to emulate a basic serial device.  It uses the baud
  * rate and sends the bytes through the loopback as approximately the
  * speed it would on a normal serial device.
  *
- * The device will appear as /dev/ttyEcho<n>.
+ * It creates two types of devices.  Echo devices simply echo back the
+ * data to the same device.  These devices will appear as /dev/ttyEcho<n>.
  *
- * You may create an arbitrary number of devices by setting the nr_ports
- * module parameter.  The default is one.
+ * Pipe devices will transfer the data between two devices.  The
+ * devices will appear as /dev/ttyPipeA<n> and /dev/ttyPipeB<n>.  And
+ * data written to PipeA reads from PipeB, and vice-versa.
+ *
+ * You may create an arbitrary number of devices by setting the
+ * nr_echo ports and nr_pipe_ports module parameters.  The default is
+ * four for both.
  */
 
 #include <linux/module.h>
@@ -25,8 +31,11 @@
 #include <linux/kthread.h>
 #include <linux/hrtimer.h>
 #include <linux/slab.h>
+#include <linux/spinlock.h>
 
 #define PORT_SERIALECHO 72549
+#define PORT_SERIALPIPEA 72550
+#define PORT_SERIALPIPEB 72551
 
 #ifdef CONFIG_HIGH_RES_TIMERS
 #define SERIALECHO_WAKES_PER_SEC	1000
@@ -35,18 +44,37 @@
 #endif
 
 #define SERIALECHO_XBUFSIZE	32
-struct serialecho_port {
+
+struct serialecho_intf {
 	struct uart_port port;
-	bool registered;
-	bool rx_enabled;
-	bool tx_enabled;
+
+	/*
+	 * This is my transmit buffer, my thread picks this up and
+	 * injects them into the other port's uart.
+	 */
 	unsigned char xmitbuf[SERIALECHO_XBUFSIZE];
 	struct circ_buf buf;
+
+	/* My transmitter is enabled. */
+	bool tx_enabled;
+
+	/* I can receive characters. */
+	bool rx_enabled;
+
+	/* Is the port registered with the uart driver? */
+	bool registered;
+
+	/*
+	 * The serial echo port on the other side of this pipe (or points
+	 * to myself in loopback mode.
+	 */
+	struct serialecho_intf *ointf;
 
 	unsigned int div;
 	unsigned int bytes_per_interval;
 	unsigned int per_interval_residual;
 
+	const char *threadname;
 	struct task_struct *thread;
 };
 
@@ -54,12 +82,16 @@ struct serialecho_port {
 					SERIALECHO_XBUFSIZE)
 #define circ_sbuf_empty(buf) ((buf)->head == (buf)->tail)
 
+static struct serialecho_intf *serialecho_port_to_intf(struct uart_port *port)
+{
+	return container_of(port, struct serialecho_intf, port);
+}
+
 static unsigned int serialecho_tx_empty(struct uart_port *port)
 {
-	struct serialecho_port *sport =
-		container_of(port, struct serialecho_port, port);
+	struct serialecho_intf *intf = serialecho_port_to_intf(port);
 
-	if (circ_sbuf_empty(&sport->buf))
+	if (circ_sbuf_empty(&intf->buf))
 		return TIOCSER_TEMT;
 	return 0;
 } 
@@ -75,13 +107,12 @@ static unsigned int serialecho_get_mctrl(struct uart_port *port)
 
 static void serialecho_stop_tx(struct uart_port *port)
 {
-	struct serialecho_port *sport =
-		container_of(port, struct serialecho_port, port);
+	struct serialecho_intf *intf = serialecho_port_to_intf(port);
 
-	sport->tx_enabled = false;
+	intf->tx_enabled = true;
 }
 
-static void serialecho_set_baud_rate(struct serialecho_port *sport,
+static void serialecho_set_baud_rate(struct serialecho_intf *intf,
 				     unsigned int baud, unsigned int cflag)
 {
 	unsigned int bits_per_char;
@@ -95,36 +126,40 @@ static void serialecho_set_baud_rate(struct serialecho_port *sport,
 	if (cflag & CSTOPB)
 		bits_per_char++;
 
-	sport->div = SERIALECHO_WAKES_PER_SEC * bits_per_char;
-	sport->bytes_per_interval = baud / sport->div;
-	sport->per_interval_residual = baud % sport->div;
+	intf->div = SERIALECHO_WAKES_PER_SEC * bits_per_char;
+	intf->bytes_per_interval = baud / intf->div;
+	intf->per_interval_residual = baud % intf->div;
 }
 
-static void serialecho_transfer_data(struct serialecho_port *sport)
+static void serialecho_transfer_data(struct serialecho_intf *intf)
 {
-	struct uart_port *port = &sport->port;
-	struct circ_buf *tbuf = &port->state->xmit;
-	struct circ_buf *rbuf = &sport->buf;
+	struct uart_port *port = &intf->port;
+	struct circ_buf *cbuf = &port->state->xmit;
+	struct circ_buf *tbuf = &intf->buf;
 
-	while (!uart_circ_empty(tbuf) && circ_sbuf_space(rbuf)) {
-		unsigned char c = tbuf->buf[tbuf->tail];
+	while (!uart_circ_empty(cbuf) && circ_sbuf_space(tbuf)) {
+		unsigned char c = cbuf->buf[cbuf->tail];
 
-		tbuf->tail = (tbuf->tail + 1) % UART_XMIT_SIZE;
-		rbuf->buf[rbuf->head] = c;
-		rbuf->head = (rbuf->head + 1) % SERIALECHO_XBUFSIZE;
+		cbuf->tail = (cbuf->tail + 1) % UART_XMIT_SIZE;
+		tbuf->buf[tbuf->head] = c;
+		tbuf->head = (tbuf->head + 1) % SERIALECHO_XBUFSIZE;
 	}
-	if (uart_circ_chars_pending(tbuf) < WAKEUP_CHARS)
+	if (uart_circ_chars_pending(cbuf) < WAKEUP_CHARS)
 		uart_write_wakeup(port);
 }
 
-static void serialecho_xmit_one(struct serialecho_port *sport)
+static unsigned int serialecho_xmit_one(struct serialecho_intf *intf)
 {
-	struct uart_port *port = &sport->port;
-	struct circ_buf *rbuf = &sport->buf;
-	unsigned char c = rbuf->buf[rbuf->tail];
+	struct uart_port *oport = &intf->ointf->port;
+	struct circ_buf *tbuf = &intf->buf;
+	unsigned char c = tbuf->buf[tbuf->tail];
 
-	rbuf->tail = (rbuf->tail + 1) % SERIALECHO_XBUFSIZE;
-	uart_insert_char(port, 0, 0, c, 0);
+	tbuf->tail = (tbuf->tail + 1) % SERIALECHO_XBUFSIZE;
+	if (intf->ointf->rx_enabled) {
+		uart_insert_char(oport, 0, 0, c, 0);
+		return 1;
+	}
+	return 0;
 }
 
 static void serialecho_thread_delay(void)
@@ -142,39 +177,44 @@ static void serialecho_thread_delay(void)
 
 static int serialecho_thread(void *data)
 {
-	struct serialecho_port *sport = data;
-	struct uart_port *port = &sport->port;
-	struct circ_buf *rbuf = &sport->buf;
+	struct serialecho_intf *intf = data;
+	struct uart_port *port = &intf->port;
+	struct uart_port *oport = &intf->ointf->port;
+	struct circ_buf *tbuf = &intf->buf;
 	unsigned int residual = 0;
 
 	while (!kthread_should_stop()) {
 		unsigned int sent = 0;
 		unsigned int to_send;
+		unsigned int div;
 
 		spin_lock_irq(&port->lock);
+		if (!intf->tx_enabled) {
+			spin_unlock_irq(&port->lock);
+			goto do_delay;
+		}
+		serialecho_transfer_data(intf);
+		to_send = intf->bytes_per_interval;
+		residual += intf->per_interval_residual;
+		div = intf->div;
+		spin_unlock_irq(&port->lock);
 
-		serialecho_transfer_data(sport);
-
-		to_send = sport->bytes_per_interval;
-		while (!circ_sbuf_empty(rbuf) && to_send) {
-			serialecho_xmit_one(sport);
-			sent++;
+		spin_lock_irq(&oport->lock);
+		while (!circ_sbuf_empty(tbuf) && to_send) {
+			sent += serialecho_xmit_one(intf);
 			to_send--;
 		}
 
-		residual += sport->per_interval_residual;
-		if (residual >= sport->div) {
-			residual -= sport->div;
-			if (!circ_sbuf_empty(rbuf)) {
-				serialecho_xmit_one(sport);
-				sent++;
-			}
+		if (residual >= div) {
+			residual -= div;
+			if (!circ_sbuf_empty(tbuf))
+				sent += serialecho_xmit_one(intf);
 		}
-		spin_unlock_irq(&port->lock);
 
 		if (sent)
-			tty_flip_buffer_push(&port->state->port);
-
+			tty_flip_buffer_push(&oport->state->port);
+		spin_unlock_irq(&oport->lock);
+	do_delay:
 		serialecho_thread_delay();
 	}
 
@@ -183,18 +223,16 @@ static int serialecho_thread(void *data)
 
 static void serialecho_start_tx(struct uart_port *port)
 {
-	struct serialecho_port *sport =
-		container_of(port, struct serialecho_port, port);
+	struct serialecho_intf *intf = serialecho_port_to_intf(port);
 
-	sport->tx_enabled = true;
+	intf->tx_enabled = true;
 }
 
 static void serialecho_stop_rx(struct uart_port *port)
 {
-	struct serialecho_port *sport =
-		container_of(port, struct serialecho_port, port);
+	struct serialecho_intf *intf = serialecho_port_to_intf(port);
 
-	sport->rx_enabled = false;
+	intf->rx_enabled = false;
 }
 
 static void serialecho_break_ctl(struct uart_port *port, int break_state)
@@ -203,34 +241,38 @@ static void serialecho_break_ctl(struct uart_port *port, int break_state)
 
 static int serialecho_startup(struct uart_port *port)
 {
-	struct serialecho_port *sport =
-		container_of(port, struct serialecho_port, port);
+	struct serialecho_intf *intf = serialecho_port_to_intf(port);
 	int rv = 0;
 
-	sport->tx_enabled = false;
-	sport->rx_enabled = true;
-
-	serialecho_set_baud_rate(sport, 9600, CS8);
-
-	sport->thread = kthread_run(serialecho_thread, sport,
-				    "serialecho%d", port->line);
-	if (IS_ERR(sport->thread)) {
-		rv = PTR_ERR(sport->thread);
-		sport->thread = NULL;
+	intf->thread = kthread_run(serialecho_thread, intf,
+				   "%s%d", intf->threadname, port->line);
+	if (IS_ERR(intf->thread)) {
+		rv = PTR_ERR(intf->thread);
+		intf->thread = NULL;
 		pr_err("serialecho: Could not start thread: %d", rv);
+	} else {
+		unsigned long flags;
+
+		spin_lock_irqsave(&port->lock, flags);
+		intf->tx_enabled = false;
+		intf->rx_enabled = true;
+
+		serialecho_set_baud_rate(intf, 9600, CS8);
+		spin_unlock_irqrestore(&port->lock, flags);
 	}
 
-	return 0;
+	return rv;
 }
 
 static void serialecho_shutdown(struct uart_port *port)
 {
-	struct serialecho_port *sport =
-		container_of(port, struct serialecho_port, port);
+	struct serialecho_intf *intf = serialecho_port_to_intf(port);
+	unsigned long flags;
 
-	kthread_stop(sport->thread);
-	sport->tx_enabled = false;
-	sport->rx_enabled = false;
+	spin_lock_irqsave(&port->lock, flags);
+	intf->tx_enabled = false;
+	spin_unlock_irqrestore(&port->lock, flags);
+	kthread_stop(intf->thread);
 }
 
 static void serialecho_release_port(struct uart_port *port)
@@ -241,31 +283,44 @@ static void
 serialecho_set_termios(struct uart_port *port, struct ktermios *termios,
 	        struct ktermios *old)
 {
-	struct serialecho_port *sport =
-		container_of(port, struct serialecho_port, port);
+	struct serialecho_intf *intf = serialecho_port_to_intf(port);
 	unsigned int baud = uart_get_baud_rate(port, termios, old,
 					       10, 100000000);
+	unsigned long flags;
 
-	spin_lock_irq(&port->lock);
-	serialecho_set_baud_rate(sport, baud, termios->c_cflag);
-	spin_unlock_irq(&port->lock);
+	spin_lock_irqsave(&port->lock, flags);
+	serialecho_set_baud_rate(intf, baud, termios->c_cflag);
+	spin_unlock_irqrestore(&port->lock, flags);
 }
 
-/**
- * serialecho_type - Describe the port.
- * @port: Ptr to the uart_port.
- *
- * Return a pointer to a string constant describing the
- * specified port.
- */
 static const char *serialecho_type(struct uart_port *port)
 {
 	return "SerialEcho";
 }
 
+static const char *serialpipea_type(struct uart_port *port)
+{
+	return "SerialPipeA";
+}
+
+static const char *serialpipeb_type(struct uart_port *port)
+{
+	return "SerialPipeB";
+}
+
 static void serialecho_config_port(struct uart_port *port, int type)
 {
 	port->type = PORT_SERIALECHO;
+}
+
+static void serialpipea_config_port(struct uart_port *port, int type)
+{
+	port->type = PORT_SERIALPIPEA;
+}
+
+static void serialpipeb_config_port(struct uart_port *port, int type)
+{
+	port->type = PORT_SERIALPIPEB;
 }
 
 static const struct uart_ops serialecho_ops = {
@@ -284,46 +339,133 @@ static const struct uart_ops serialecho_ops = {
 	.config_port =		serialecho_config_port
 };
 
+static const struct uart_ops serialpipea_ops = {
+	.tx_empty =		serialecho_tx_empty,
+	.set_mctrl =		serialecho_set_mctrl,
+	.get_mctrl =		serialecho_get_mctrl,
+	.stop_tx =		serialecho_stop_tx,
+	.start_tx =		serialecho_start_tx,
+	.stop_rx =		serialecho_stop_rx,
+	.break_ctl =		serialecho_break_ctl,
+	.startup =		serialecho_startup,
+	.shutdown =		serialecho_shutdown,
+	.release_port =		serialecho_release_port,
+	.set_termios =		serialecho_set_termios,
+	.type =			serialpipea_type,
+	.config_port =		serialpipea_config_port
+};
+
+static const struct uart_ops serialpipeb_ops = {
+	.tx_empty =		serialecho_tx_empty,
+	.set_mctrl =		serialecho_set_mctrl,
+	.get_mctrl =		serialecho_get_mctrl,
+	.stop_tx =		serialecho_stop_tx,
+	.start_tx =		serialecho_start_tx,
+	.stop_rx =		serialecho_stop_rx,
+	.break_ctl =		serialecho_break_ctl,
+	.startup =		serialecho_startup,
+	.shutdown =		serialecho_shutdown,
+	.release_port =		serialecho_release_port,
+	.set_termios =		serialecho_set_termios,
+	.type =			serialpipeb_type,
+	.config_port =		serialpipeb_config_port
+};
+
 static struct uart_driver serialecho_driver = {
 	.owner = THIS_MODULE,
 	.driver_name = "ttyEcho",
 	.dev_name = "ttyEcho"
 };
 
+static struct uart_driver serialpipea_driver = {
+	.owner = THIS_MODULE,
+	.driver_name = "ttyPipeA",
+	.dev_name = "ttyPipeA"
+};
 
-static unsigned int nr_ports = 1;
-module_param(nr_ports, uint, 0444);
-MODULE_PARM_DESC(nr_ports, "The number of ports to create.  Defaults to 1");
+static struct uart_driver serialpipeb_driver = {
+	.owner = THIS_MODULE,
+	.driver_name = "ttyPipeB",
+	.dev_name = "ttyPipeB"
+};
 
-static struct serialecho_port *serialecho_ports;
+
+static unsigned int nr_echo_ports = 4;
+module_param(nr_echo_ports, uint, 0444);
+MODULE_PARM_DESC(nr_echo_ports,
+		 "The number of echo ports to create.  Defaults to 4");
+
+static struct serialecho_intf *serialecho_ports;
+
+static unsigned int nr_pipe_ports = 4;
+module_param(nr_pipe_ports, uint, 0444);
+MODULE_PARM_DESC(nr_pipe_ports,
+		 "The number of pipe ports to create.  Defaults to 4");
+
+static struct serialecho_intf *serialecho_ports;
+static struct serialecho_intf *serialpipe_ports;
 
 static int __init serialecho_init(void)
 {
 	unsigned int i;
 	int rv;
 
-	serialecho_ports = kcalloc(nr_ports,
-				   sizeof(struct uart_port),
+	serialecho_ports = kcalloc(nr_echo_ports,
+				   sizeof(*serialecho_ports),
 				   GFP_KERNEL);
 	if (!serialecho_ports) {
-		pr_err("serialecho: Unable to allocate ports.\n");
+		pr_err("serialecho: Unable to allocate echo ports.\n");
 		rv = ENOMEM;
 		goto out;
 	}
 
-	serialecho_driver.nr = nr_ports;
+	serialpipe_ports = kcalloc(nr_pipe_ports * 2,
+				   sizeof(*serialpipe_ports),
+				   GFP_KERNEL);
+	if (!serialpipe_ports) {
+		kfree(serialecho_ports);
+		pr_err("serialecho: Unable to allocate pipe ports.\n");
+		rv = ENOMEM;
+		goto out;
+	}
+
+	serialecho_driver.nr = nr_echo_ports;
 	rv = uart_register_driver(&serialecho_driver);
 	if (rv) {
 		kfree(serialecho_ports);
+		kfree(serialpipe_ports);
 		pr_err("serialecho: Unable to register driver.\n");
 		goto out;
 	}
 
-	for (i = 0; i < nr_ports; i++) {
-		struct serialecho_port *sport = &serialecho_ports[i];
-		struct uart_port *port = &sport->port;
+	serialpipea_driver.nr = nr_pipe_ports;
+	rv = uart_register_driver(&serialpipea_driver);
+	if (rv) {
+		uart_unregister_driver(&serialecho_driver);
+		kfree(serialecho_ports);
+		kfree(serialpipe_ports);
+		pr_err("serialecho: Unable to register driver.\n");
+		goto out;
+	}
 
-		sport->buf.buf = sport->xmitbuf;
+	serialpipeb_driver.nr = nr_pipe_ports;
+	rv = uart_register_driver(&serialpipeb_driver);
+	if (rv) {
+		uart_unregister_driver(&serialpipea_driver);
+		uart_unregister_driver(&serialecho_driver);
+		kfree(serialecho_ports);
+		kfree(serialpipe_ports);
+		pr_err("serialecho: Unable to register driver.\n");
+		goto out;
+	}
+
+	for (i = 0; i < nr_echo_ports; i++) {
+		struct serialecho_intf *intf = &serialecho_ports[i];
+		struct uart_port *port = &intf->port;
+
+		intf->buf.buf = intf->xmitbuf;
+		intf->ointf = intf;
+		intf->threadname = "serialecho";
 		/* Won't configure without some I/O or mem address set. */
 		port->iobase = 1;
 		port->line = i;
@@ -335,7 +477,51 @@ static int __init serialecho_init(void)
 			pr_err("serialecho: Unable to add uart port %d: %d\n",
 			       i, rv);
 		else
-			sport->registered = true;
+			intf->registered = true;
+	}
+
+	for (i = 0; i < nr_pipe_ports * 2; i += 2) {
+		struct serialecho_intf *intfa = &serialpipe_ports[i];
+		struct serialecho_intf *intfb = &serialpipe_ports[i + 1];
+		struct uart_port *porta = &intfa->port;
+		struct uart_port *portb = &intfb->port;
+
+		intfa->buf.buf = intfa->xmitbuf;
+		intfb->buf.buf = intfb->xmitbuf;
+		intfa->ointf = intfb;
+		intfb->ointf = intfa;
+		intfa->threadname = "serialpipea";
+		intfb->threadname = "serialpipeb";
+
+		/* Won't configure without some I/O or mem address set. */
+		porta->iobase = 1;
+		porta->line = i / 2;
+		porta->flags = UPF_BOOT_AUTOCONF;
+		porta->ops = &serialpipea_ops;
+		spin_lock_init(&porta->lock);
+		rv = uart_add_one_port(&serialpipea_driver, porta);
+		if (rv) {
+			pr_err("serialecho: Unable to add uart pipe aport %d: %d\n",
+			       i, rv);
+			continue;
+		} else {
+			intfa->registered = true;
+		}
+
+		portb->iobase = 1;
+		portb->line = i / 2;
+		portb->flags = UPF_BOOT_AUTOCONF;
+		portb->ops = &serialpipeb_ops;
+		spin_lock_init(&portb->lock);
+		rv = uart_add_one_port(&serialpipeb_driver, portb);
+		if (rv) {
+			pr_err("serialecho: Unable to add uart pipe b port %d: %d\n",
+			       i, rv);
+			intfa->registered = false;
+			uart_remove_one_port(&serialpipea_driver, porta);
+		} else {
+			intfb->registered = true;
+		}
 	}
 	rv = 0;
 
@@ -348,14 +534,32 @@ static void __exit serialecho_exit(void)
 {
 	unsigned int i;
 
-	for (i = 0; i < nr_ports; i++) {
-		struct serialecho_port *sport = &serialecho_ports[i];
-		struct uart_port *port = &sport->port;
+	for (i = 0; i < nr_echo_ports; i++) {
+		struct serialecho_intf *intf = &serialecho_ports[i];
+		struct uart_port *port = &intf->port;
 
-		if (sport->registered)
+		if (intf->registered)
 			uart_remove_one_port(&serialecho_driver, port);
 	}
+
+	for (i = 0; i < nr_pipe_ports * 2; i += 2) {
+		struct serialecho_intf *intfa = &serialpipe_ports[i];
+		struct serialecho_intf *intfb = &serialpipe_ports[i + 1];
+		struct uart_port *porta = &intfa->port;
+		struct uart_port *portb = &intfb->port;
+
+		if (intfa->registered)
+			uart_remove_one_port(&serialpipea_driver, porta);
+		if (intfb->registered)
+			uart_remove_one_port(&serialpipeb_driver, portb);
+	}
 	uart_unregister_driver(&serialecho_driver);
+	uart_unregister_driver(&serialpipea_driver);
+	uart_unregister_driver(&serialpipeb_driver);
+
+	kfree(serialecho_ports);
+	kfree(serialpipe_ports);
+
 	pr_info("serialecho unloaded\n");
 }
 
