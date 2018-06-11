c9d6b61d1863846a445d6a91f2ffe099c03b6551
Corey Minyard <cminyard@mvista.com>
2018-06-11 07:53:50 -0500

    serialsim: Add the ability to control mctrl and errors via ioctl
    On the pipe interface, this adds an ioctl that lets you control
    modem control and inject errors on the other end of the pipe.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

---
+++ linux-serial-echo/serialsim.h
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0+
+
+/*
+ * serialsim - Emulate a serial device in a loopback and/or pipe
+ */
+
+/*
+ * TTY IOCTLs for controlling the modem control and for error injection.
+ * See serialsim.c for details.
+ */
+
+#ifndef LINUX_SERIALSIM_H
+#define LINUX_SERIALSIM_H
+
+#define TIOCSERSNULLMODEM	0x54e4
+#define TIOCSERSREMMCTRL	0x54e5
+#define TIOCSERSREMERR		0x54e6
+
+#endif
