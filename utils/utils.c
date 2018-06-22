8298bc120377006554722cb348abe52ccbfe8f94
Corey Minyard <cminyard@mvista.com>
2018-06-22 16:52:08 -0500

    Clean up baud rate settings
    Handle all the baud rates in utils.h, and add a string function
    there, too, to remove a lot of unnecessary stuff from devio_devcfg.c.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- utils/utils.c
+++ utils/utils.c
@@ -271,29 +271,67 @@ int str_to_argv(const char *ins, int *r_argc, char ***r_argv, char *seps)
 static struct baud_rates_s {
     int real_rate;
     int val;
+    const char *str;
 } baud_rates[] =
 {
-    { 50, B50 },
-    { 75, B75 },
-    { 110, B110 },
-    { 134, B134 },
-    { 150, B150 },
-    { 200, B200 },
-    { 300, B300 },
-    { 600, B600 },
-    { 1200, B1200 },
-    { 1800, B1800 },
-    { 2400, B2400 },
-    { 4800, B4800 },
-    { 9600, B9600 },
+    { 50, B50, "50" },
+    { 75, B75, "75" },
+    { 110, B110, "110" },
+    { 134, B134, "134" },
+    { 150, B150, "150" },
+    { 200, B200, "200" },
+    { 300, B300, "300" },
+    { 600, B600, "600" },
+    { 1200, B1200, "1200" },
+    { 1800, B1800, "1800" },
+    { 2400, B2400, "2400" },
+    { 4800, B4800, "4800" },
+    { 9600, B9600, "9600" },
     /* We don't support 14400 baud */
-    { 19200, B19200 },
+    { 19200, B19200, "19200" },
     /* We don't support 28800 baud */
-    { 38400, B38400 },
-    { 57600, B57600 },
-    { 115200, B115200 },
-    { 230400, B230400 },
-    /* We don't support 460800 baud */
+    { 38400, B38400, "38400" },
+    { 57600, B57600, "57600" },
+    { 115200, B115200, "115200" },
+#ifdef B230400
+    { 230400, B230400, "230400" },
+#endif
+#ifdef B460800
+    { 460800, B460800, "460800" },
+#endif
+#ifdef B500000
+    { 500000, B500000, "500000" },
+#endif
+#ifdef B576000
+    { 576000, B576000, "576000" },
+#endif
+#ifdef B921600
+    { 921600, B921600, "921600" },
+#endif
+#ifdef B1000000
+    { 1000000, B1000000, "1000000" },
+#endif
+#ifdef B1152000
+    { 1152000, B1152000, "1152000" },
+#endif
+#ifdef B1500000
+    { 1500000, B1500000, "1500000" },
+#endif
+#ifdef B2000000
+    { 2000000, B2000000, "2000000" },
+#endif
+#ifdef B2500000
+    { 2500000, B2500000, "2500000" },
+#endif
+#ifdef B3000000
+    { 3000000, B3000000, "3000000" },
+#endif
+#ifdef B3500000
+    { 3500000, B3500000, "3500000" },
+#endif
+#ifdef B4000000
+    { 4000000, B4000000, "4000000" },
+#endif
 };
 #define BAUD_RATES_LEN ((sizeof(baud_rates) / sizeof(struct baud_rates_s)))
 
@@ -310,6 +348,18 @@ get_baud_rate(int rate, int *val)
     }
 
     return 0;
+}
+
+const char *
+get_baud_rate_str(int baud_rate)
+{
+    unsigned int i;
+    for (i = 0; i < BAUD_RATES_LEN; i++) {
+	if (baud_rate == baud_rates[i].val)
+	    return baud_rates[i].str;
+    }
+
+    return "unknown speed";
 }
 
 void
