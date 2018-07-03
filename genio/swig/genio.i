20ab1ebc4318216895754cf12cc147092a955800
Corey Minyard <cminyard@mvista.com>
2018-07-03 13:28:58 -0500

    genio: Add a swig python setup
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

---
+++ genio/swig/genio.i
@@ -0,0 +1,219 @@
+/*
+ *  ser2net - A program for allowing telnet connection to serial ports
+ *  Copyright (C) 2018  Corey Minyard <minyard@acm.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+%module genio
+
+%{
+#include <string.h>
+#include "genio/genio.h"
+#include "utils/selector.h"
+#include "utils/waiter.h"
+
+static char *err_to_errstr(int err)
+{
+    if (!err)
+	return "";
+    return strerror(err);
+}
+
+#include "genio_python.h"
+
+static struct selector_s *genio_sel;
+
+static void setup_genio_sel(void)
+{
+    if (!genio_sel)
+	sel_alloc_selector_nothread(&genio_sel);
+}
+
+static int
+genio_do_wait(struct waiter_s *waiter, struct timeval *timeout)
+{
+    int err;
+
+    do {
+	err = wait_for_waiter_timeout_intr(waiter, 1, timeout);
+	if (check_for_err())
+	    break;
+	if (err)
+	    break;
+    } while (1);
+
+    return err;
+}
+
+%}
+
+%include "genio_python.i"
+
+%include <typemaps.i>
+
+struct genio { };
+struct genio_acceptor { };
+struct waiter_s { };
+
+%extend genio {
+    genio(char *str, int max_read_size, swig_cb *handler) {
+	int rv;
+	struct genio_data *data;
+	struct genio *io = NULL;
+
+	setup_genio_sel();
+
+	data = malloc(sizeof(*data));
+	if (!data)
+	    return NULL;
+
+	data->handler_val = ref_swig_cb(handler, read_callback);
+
+	rv = str_to_genio(str, genio_sel, max_read_size, &gen_cbs,
+			  data, &io);
+	if (rv) {
+	    deref_swig_cb_val(data->handler_val);
+	    free(data);
+	}
+			  
+	return io;
+    }
+
+    ~genio()
+    {
+	struct genio_data *data = genio_get_user_data(self);
+
+	genio_free(self);
+	deref_swig_cb_val(data->handler_val);
+	free(data);
+    }
+
+    %rename(open) opent;
+    char *opent() {
+	return err_to_errstr(genio_open(self));
+    }
+
+    %rename(close) closet;
+    char *closet(swig_cb *done) {
+	swig_cb_val *done_val = NULL;
+	void (*close_done)(struct genio *io, void *cb_data) = NULL;
+	int rv;
+	
+	if (!nil_swig_cb(done)) {
+	    close_done = genio_close_done;
+	    done_val = ref_swig_cb(done, close_done);
+	}
+	rv = genio_close(self, close_done, done_val);
+	if (rv && done_val)
+	    deref_swig_cb_val(done_val);
+
+	return err_to_errstr(rv);
+    }
+
+    %rename(write) writet;
+    %apply (char *STRING, size_t LENGTH) { (char *str, size_t len) };
+    %apply int *OUTPUT { int *written };
+    char *writet(int *written, char *str, size_t len) {
+	unsigned int wr = 0;
+	int rv;
+
+	rv = genio_write(self, &wr, str, len);
+	*written = wr;
+	return err_to_errstr(rv);
+    }
+
+    void read_cb_enable(bool enable) {
+	genio_set_read_callback_enable(self, enable);
+    }
+
+    void write_cb_enable(bool enable) {
+	genio_set_write_callback_enable(self, enable);
+    }
+}
+
+%nodefaultctor genio_acceptor;
+%extend genio_acceptor {
+    genio_acceptor(char *str, int max_read_size, swig_cb *handler) {
+	struct genio_acc_data *data;
+	struct genio_acceptor *acc;
+	int rv;
+
+	setup_genio_sel();
+
+	data = malloc(sizeof(*data));
+	if (!data)
+	    return NULL;
+
+	data->handler_val = ref_swig_cb(handler, new_connection);
+
+	rv = str_to_genio_acceptor(str, genio_sel, max_read_size, &gen_acc_cbs,
+			  data, &acc);
+	if (rv) {
+	    deref_swig_cb_val(data->handler_val);
+	    free(data);
+	}
+
+	return acc;
+    }
+
+    ~genio_acceptor()
+    {
+	struct genio_acc_data *data = genio_acceptor_get_user_data(self);
+
+	genio_acc_free(self);
+	deref_swig_cb_val(data->handler_val);
+	free(data);
+    }
+
+    char *shutdown(swig_cb *done) {
+	swig_cb_val *done_val = NULL;
+	int rv;
+
+	if (!nil_swig_cb(done))
+	    done_val = ref_swig_cb(done, shutdown);
+	rv = genio_acc_shutdown(self, genio_acc_shutdown_done, done_val);
+	if (rv && done_val)
+	    deref_swig_cb_val(done_val);
+
+	return err_to_errstr(rv);
+    }
+}
+
+%nodefaultctor genio_acceptor;
+%extend waiter_s {
+    waiter_s() {
+	setup_genio_sel();
+	return alloc_waiter(genio_sel, 0);
+    }
+
+    ~waiter_s() {
+	free_waiter(self);
+    }
+
+    void wait_timeout(int timeout) {
+	struct timeval tv = { timeout / 1000, timeout % 1000 };
+
+	genio_do_wait(self, &tv);
+    }
+
+    void wait() {
+	genio_do_wait(self, NULL);
+    }
+
+    void wake() {
+	wake_waiter(self);
+    }
+}
---
+++ genio/swig/python/genio_python.h
@@ -0,0 +1,322 @@
+/*
+ *  ser2net - A program for allowing telnet connection to serial ports
+ *  Copyright (C) 2018  Corey Minyard <minyard@acm.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+typedef PyObject swig_cb;
+typedef PyObject swig_cb_val;
+typedef struct swig_ref {
+    PyObject *val;
+} swig_ref;
+
+#define nil_swig_cb(v) ((v) == NULL)
+#define invalidate_swig_cb(v) ((v) = NULL)
+
+#define OI_PY_STATE int
+#define OI_PY_STATE_GET() 0
+#define OI_PY_STATE_PUT(s) do { } while(s)
+
+static swig_cb_val *
+ref_swig_cb_i(swig_cb *cb)
+{
+    OI_PY_STATE gstate;
+
+    gstate = OI_PY_STATE_GET();
+    Py_INCREF(cb);
+    OI_PY_STATE_PUT(gstate);
+    return cb;
+}
+#define ref_swig_cb(cb, func) ref_swig_cb_i(cb)
+
+static swig_ref
+swig_make_ref_i(void *item, swig_type_info *class)
+{
+    swig_ref    rv;
+    OI_PY_STATE gstate;
+
+    gstate = OI_PY_STATE_GET();
+    rv.val = SWIG_NewPointerObj(item, class, 0);
+    OI_PY_STATE_PUT(gstate);
+    return rv;
+}
+#define swig_make_ref(item, name) \
+	swig_make_ref_i(item, SWIGTYPE_p_ ## name)
+
+static void
+swig_free_ref(swig_ref ref)
+{
+    OI_PY_STATE gstate;
+
+    gstate = OI_PY_STATE_GET();
+    Py_DECREF(ref.val);
+    OI_PY_STATE_PUT(gstate);
+}
+
+static swig_cb_val *
+deref_swig_cb_val(swig_cb_val *cb)
+{
+    OI_PY_STATE gstate;
+
+    gstate = OI_PY_STATE_GET();
+    Py_DECREF(cb);
+    OI_PY_STATE_PUT(gstate);
+    return cb;
+}
+
+/* No way to check the refcount in Python. */
+#define swig_free_ref_check(r, c) \
+	do {								\
+	    swig_free_ref(r);						\
+	} while(0)
+
+static PyObject *
+swig_finish_call_rv(swig_cb_val *cb, char *method_name, PyObject *args)
+{
+    PyObject *p, *o = NULL;
+
+    p = PyObject_GetAttrString(cb, method_name);
+    if (p) {
+	o = PyObject_CallObject(p, args);
+	Py_DECREF(p);
+	if (PyErr_Occurred()) {
+	    PyErr_Print();
+	    exit(1);
+	}
+    } else {
+	PyObject *t = PyObject_GetAttrString(cb, "__class__");
+	PyObject *c = PyObject_GetAttrString(t, "__name__");
+	char *class = PyString_AsString(c);
+
+	fprintf(stderr, "genio callback: Class '%s' has no method '%s'\n",
+		class, method_name);
+	exit(1);
+    }
+    Py_DECREF(args);
+
+    return o;
+}
+
+static void
+swig_finish_call(swig_cb_val *cb, char *method_name, PyObject *args)
+{
+    PyObject *o;
+
+    o = swig_finish_call_rv(cb, method_name, args);
+    if (o)
+	Py_DECREF(o);
+}
+
+#if PY_VERSION_HEX >= 0x03000000
+#define OI_PI_FromStringAndSize PyUnicode_FromStringAndSize
+#else
+#define OI_PI_FromStringAndSize PyString_FromStringAndSize
+#endif
+
+struct genio_data {
+    swig_cb_val *handler_val;
+};
+
+static void
+genio_close_done(struct genio *io, void *cb_data) {
+    swig_cb_val *cb = cb_data;
+    swig_ref io_ref;
+    PyObject *args;
+    OI_PY_STATE gstate;
+
+    gstate = OI_PY_STATE_GET();
+
+    io_ref = swig_make_ref(io, genio);
+    args = PyTuple_New(1);
+    Py_INCREF(io_ref.val);
+    PyTuple_SET_ITEM(args, 0, io_ref.val);
+
+    swig_finish_call(cb, "close_done", args);
+
+    swig_free_ref_check(io_ref, acceptor);
+    deref_swig_cb_val(cb);
+    OI_PY_STATE_PUT(gstate);
+}
+
+static unsigned int
+genio_got_read(struct genio *io, int readerr,
+	       unsigned char *buf, unsigned int buflen,
+	       unsigned int flags)
+{
+    struct genio_data *data = genio_get_user_data(io);
+    swig_ref io_ref;
+    PyObject *args, *o;
+    OI_PY_STATE gstate;
+    unsigned int rv;
+
+    gstate = OI_PY_STATE_GET();
+
+    args = PyTuple_New(4);
+
+    io_ref = swig_make_ref(io, genio);
+    Py_INCREF(io_ref.val);
+    PyTuple_SET_ITEM(args, 0, io_ref.val);
+
+    if (readerr)
+	o = PyString_FromString(err_to_errstr(readerr));
+    else
+	o = Py_None;
+    PyTuple_SET_ITEM(args, 1, o);
+
+    o = OI_PI_FromStringAndSize((char *) buf, buflen);
+    PyTuple_SET_ITEM(args, 2, o);
+
+    o = PyInt_FromLong(flags);
+    PyTuple_SET_ITEM(args, 3, o);
+
+    o = swig_finish_call_rv(data->handler_val, "read_callback", args);
+    if (!o) {
+	PyObject *t = PyObject_GetAttrString(data->handler_val, "__class__");
+	PyObject *c = PyObject_GetAttrString(t, "__name__");
+	char *class = PyString_AsString(c);
+
+	fprintf(stderr, "genio callback: "
+		"Class '%s' method 'read_callback' did not return a value\n",
+		class);
+	exit(1);
+    } else {
+	rv = PyLong_AsUnsignedLong(o);
+	if (PyErr_Occurred()) {
+	    PyObject *t = PyObject_GetAttrString(data->handler_val,
+						 "__class__");
+	    PyObject *c = PyObject_GetAttrString(t, "__name__");
+	    char *class = PyString_AsString(c);
+
+	    fprintf(stderr, "genio callback: "
+		    "Class '%s' method 'read_callback' did not return "
+		    "an integer\n",
+		    class);
+	    exit(1);
+	}
+	Py_DECREF(o);
+    }
+
+    swig_free_ref_check(io_ref, acceptor);
+    OI_PY_STATE_PUT(gstate);
+
+    return rv;
+}
+
+static void
+genio_write_read(struct genio *io)
+{
+    struct genio_data *data = genio_get_user_data(io);
+    swig_ref io_ref;
+    PyObject *args;
+    OI_PY_STATE gstate;
+
+    gstate = OI_PY_STATE_GET();
+
+    io_ref = swig_make_ref(io, genio);
+    args = PyTuple_New(1);
+    Py_INCREF(io_ref.val);
+    PyTuple_SET_ITEM(args, 0, io_ref.val);
+
+    swig_finish_call(data->handler_val, "write_callback", args);
+
+    swig_free_ref_check(io_ref, acceptor);
+    OI_PY_STATE_PUT(gstate);
+}
+
+static void
+genio_got_urgent(struct genio *io)
+{
+    struct genio_data *data = genio_get_user_data(io);
+    swig_ref io_ref;
+    PyObject *args;
+    OI_PY_STATE gstate;
+
+    gstate = OI_PY_STATE_GET();
+
+    io_ref = swig_make_ref(io, genio);
+    args = PyTuple_New(1);
+    Py_INCREF(io_ref.val);
+    PyTuple_SET_ITEM(args, 0, io_ref.val);
+
+    swig_finish_call(data->handler_val, "urgent_callback", args);
+
+    swig_free_ref_check(io_ref, acceptor);
+    OI_PY_STATE_PUT(gstate);
+}
+
+static struct genio_callbacks gen_cbs = {
+    .read_callback = genio_got_read,
+    .write_callback = genio_write_read,
+    .urgent_callback = genio_got_urgent
+};
+
+struct genio_acc_data {
+    swig_cb_val *handler_val;
+};
+
+static void
+genio_acc_shutdown_done(struct genio_acceptor *acceptor, void *cb_data)
+{
+    swig_cb_val *cb = cb_data;
+    swig_ref acc_ref;
+    PyObject *args;
+    OI_PY_STATE gstate;
+
+    gstate = OI_PY_STATE_GET();
+
+    acc_ref = swig_make_ref(acceptor, genio_acceptor);
+    args = PyTuple_New(1);
+    Py_INCREF(acc_ref.val);
+    PyTuple_SET_ITEM(args, 0, acc_ref.val);
+
+    swig_finish_call(cb, "shutdown_done", args);
+
+    swig_free_ref_check(acc_ref, acceptor);
+    deref_swig_cb_val(cb);
+    OI_PY_STATE_PUT(gstate);
+}
+
+static void
+genio_acc_got_new(struct genio_acceptor *acceptor, struct genio *io)
+{
+    struct genio_acc_data *data = genio_acceptor_get_user_data(acceptor);
+    swig_ref acc_ref, io_ref;
+    PyObject *args;
+    OI_PY_STATE gstate;
+
+    gstate = OI_PY_STATE_GET();
+
+    acc_ref = swig_make_ref(acceptor, genio_acceptor);
+    io_ref = swig_make_ref(io, genio);
+    args = PyTuple_New(2);
+    Py_INCREF(acc_ref.val);
+    Py_INCREF(io_ref.val);
+    PyTuple_SET_ITEM(args, 0, acc_ref.val);
+    PyTuple_SET_ITEM(args, 1, io_ref.val);
+
+    swig_finish_call(data->handler_val, "new_connection", args);
+
+    swig_free_ref_check(acc_ref, acceptor);
+    swig_free_ref_check(io_ref, acceptor);
+    OI_PY_STATE_PUT(gstate);
+}
+
+static struct genio_acceptor_callbacks gen_acc_cbs = {
+    .new_connection = genio_acc_got_new
+};
+
+#define check_for_err PyErr_Occurred
+
