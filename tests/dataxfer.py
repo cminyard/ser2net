5ce0a3eb22362feb120fc2982f7c6c187d2bf35b
Corey Minyard <cminyard@mvista.com>
2018-07-09 16:26:26 -0500

    tests: Fix more python3 issues
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- tests/dataxfer.py
+++ tests/dataxfer.py
@@ -7,17 +7,17 @@ def test_transfer(name, data, config, io1str, io2str, timeout=1000):
     This function takes a name (for reporting), some data to transfer,
     a config file for ser2net, and 
     """
-    print "Transfer %s:\n  config=%s  io1=%s\n  io2=%s" % (
-        name, config, io1str, io2str)
+    print("Transfer %s:\n  config=%s  io1=%s\n  io2=%s" %
+          (name, config, io1str, io2str))
 
     ser2net, io1, io2 = utils.setup_2_ser2net(config, io1str, io2str)
 
-    print "  io1 to io2"
+    print("  io1 to io2")
     utils.test_dataxfer(io1, io2, data, timeout=timeout)
-    print "  io2 to io1"
+    print("  io2 to io1")
     utils.test_dataxfer(io2, io1, data, timeout=timeout)
-    print "  bidirectional"
+    print("  bidirectional")
     utils.test_dataxfer_simul(io1, io2, data, timeout=timeout)
 
     utils.finish_2_ser2net(ser2net, io1, io2)
-    print "  Success!"
+    print("  Success!")
--- tests/test_genio.py
+++ tests/test_genio.py
@@ -2,12 +2,12 @@
 import utils
 
 def t1():
-    print "Test1"
+    print("Test1")
     io = utils.alloc_io("ser,termios,/dev/ttyEcho0,38400")
     utils.test_dataxfer(io, io, "This is a test string!")
 
 def t2():
-    print "Test2"
+    print("Test2")
     io1 = utils.alloc_io("ser,termios,/dev/ttyPipeA0,9600")
     io2 = utils.alloc_io("ser,termios,/dev/ttyPipeB0,9600")
     utils.test_dataxfer(io1, io2, "This is a test string!")
