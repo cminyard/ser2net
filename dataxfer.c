e521637854f5c95c9b2a948f376138e865ff9b1f
Corey Minyard <cminyard@mvista.com>
2018-09-08 22:29:54 -0500

    Set "sent will" for telnet binary transmission
    We sent a will, make sure we mark it so in the telnet command table.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- dataxfer.c
+++ dataxfer.c
@@ -397,7 +397,7 @@ static struct telnet_cmd telnet_cmds[] =
     /*                        I will,  I do,  sent will, sent do */
     { TN_OPT_SUPPRESS_GO_AHEAD,	   0,     1,          1,       0, },
     { TN_OPT_ECHO,		   0,     1,          1,       1, },
-    { TN_OPT_BINARY_TRANSMISSION,  1,     1,          0,       1, },
+    { TN_OPT_BINARY_TRANSMISSION,  1,     1,          1,       1, },
     { TN_OPT_COM_PORT,		   1,     1,          0,       0, 0, 0,
       com_port_handler, com_port_will },
     { TELNET_CMD_END_OPTION }
