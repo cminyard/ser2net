5676d4533297099ca1327f14b13ca283418c6e6a
Corey Minyard <cminyard@mvista.com>
2019-01-07 12:41:28 -0600

    Fixes for small SSL semantics changes
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- tests/test_xfer_large_ssl_tcp.py
+++ tests/test_xfer_large_ssl_tcp.py
@@ -7,7 +7,7 @@ import utils
 rb = gensio.get_random_bytes(1048576)
 
 test_transfer("ssl tcp large random", rb,
-              "ssl(key=%s/key.pem,cert=%s/cert.pem,CA=%s/CA.pem),3023:raw:100:/dev/ttyPipeA0:115200N81\n" % (utils.srcdir, utils.srcdir, utils.srcdir),
+              "ssl(key=%s/key.pem,cert=%s/cert.pem),3023:raw:100:/dev/ttyPipeA0:115200N81\n" % (utils.srcdir, utils.srcdir),
               "ssl(CA=%s/CA.pem),tcp,localhost,3023" % utils.srcdir,
               "serialdev,/dev/ttyPipeB0,115200N81",
               timeout=100000)
--- tests/test_xfer_small_ssl_tcp.py
+++ tests/test_xfer_small_ssl_tcp.py
@@ -7,7 +7,7 @@ import utils
 rb = gensio.get_random_bytes(512)
 
 test_transfer("ssl tcp small random", rb,
-              "ssl(key=%s/key.pem,cert=%s/cert.pem,CA=%s/CA.pem),3023:raw:100:/dev/ttyPipeA0:9600N81\n" % (utils.srcdir, utils.srcdir, utils.srcdir),
+              "ssl(key=%s/key.pem,cert=%s/cert.pem),3023:raw:100:/dev/ttyPipeA0:9600N81\n" % (utils.srcdir, utils.srcdir),
               "ssl(CA=%s/CA.pem),tcp,localhost,3023" % utils.srcdir,
               "serialdev,/dev/ttyPipeB0,9600N81",
               timeout=5000)
