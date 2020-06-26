7c8827a5549b6ab69d4fe2e33da6d149791571cc
Corey Minyard <cminyard@mvista.com>
2020-06-26 15:17:10 -0500

    tests: Add a large udp test
    Now that we have relpkt, we can implement a large udp test.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

---
+++ tests/test_xfer_large_udp.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import os
+import gensio
+from dataxfer import test_transfer
+
+rb = os.urandom(104857)
+
+test_transfer("udp large random", rb,
+              ("connection: &con",
+               "  accepter: relpkt,udp,3023",
+               "  connector: serialdev,/dev/ttyPipeA1,115200n81"),
+              "relpkt,udp,localhost,3023",
+              "serialdev,/dev/ttyPipeB1,115200N81",
+              timeout=200000)
