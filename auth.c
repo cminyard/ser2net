06cf2a20b4bce1b889bc4dbf244ea126c2efac6b
Corey Minyard <cminyard@mvista.com>
2020-10-06 07:23:21 -0500

    Add OpenSSL license exception
    There is some debate about whether the OpenSSL license is compatible
    with the GPL.  In order to avoid any issues, I've added a license
    exception for OpenSSL.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- auth.c
+++ auth.c
@@ -3,6 +3,24 @@
  *  Copyright (C) 2001=2020  Corey Minyard <minyard@acm.org>
  *
  *  SPDX-License-Identifier: GPL-2.0-only
+ *
+ *  In addition, as a special exception, the copyright holders of
+ *  ser2net give you permission to combine ser2net with free software
+ *  programs or libraries that are released under the GNU LGPL and
+ *  with code included in the standard release of OpenSSL under the
+ *  OpenSSL license (or modified versions of such code, with unchanged
+ *  license). You may copy and distribute such a system following the
+ *  terms of the GNU GPL for ser2net and the licenses of the other code
+ *  concerned, provided that you include the source code of that
+ *  other code when and as the GNU GPL requires distribution of source
+ *  code.
+ *
+ *  Note that people who make modified versions of ser2net are not
+ *  obligated to grant this special exception for their modified
+ *  versions; it is their choice whether to do so. The GNU General
+ *  Public License gives permission to release a modified version
+ *  without this exception; this exception also makes it possible to
+ *  release a modified version which carries forward this exception.
  */
 
 #include <stdio.h>
